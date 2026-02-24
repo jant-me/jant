797bec092b4f0d73ac2228f7ee920f114fe6fdd9
Owen <62473795+theowenyoung@users.noreply.github.com>
2026-02-24 09:19:08 +0800

    search

--- packages/core/src/ui/compose/ComposePrompt.tsx
+++ packages/core/src/ui/compose/ComposePrompt.tsx
@@ -21,8 +21,8 @@ export const ComposePrompt: FC = () => {
         <span class="compose-prompt-avatar">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
+            width="18"
+            height="18"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
@@ -30,7 +30,9 @@ export const ComposePrompt: FC = () => {
             stroke-linecap="round"
             stroke-linejoin="round"
           >
-            <path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 1 1 3.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+            <path d="M20.24 12.24a6 6 0 0 0-8.49-8.49L5 10.5V19h8.5z" />
+            <line x1="16" y1="8" x2="2" y2="22" />
+            <line x1="17.5" y1="15" x2="9" y2="15" />
           </svg>
         </span>
         <span class="compose-prompt-text">
